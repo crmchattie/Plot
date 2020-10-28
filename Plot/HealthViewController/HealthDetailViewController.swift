fb08afe585da42573711f7a3703d148a8fd0aacd
Hafiz Usama <hafizuamjad@gmail.com>
2020-10-28 00:32:30 -0700

    Update map to use cubic style

--- Plot/HealthViewController/HealthDetailViewController.swift
+++ Plot/HealthViewController/HealthDetailViewController.swift
@@ -15,8 +15,8 @@ class HealthDetailViewController: UIViewController {
     var healthMetric: HealthMetric?
     var dayAxisValueFormatter: DayAxisValueFormatter?
     
-    lazy var chartView: BarChartView = {
-        let chartView = BarChartView()
+    lazy var chartView: LineChartView = {
+        let chartView = LineChartView()
         chartView.translatesAutoresizingMaskIntoConstraints = false
         return chartView
     }()
@@ -74,108 +74,40 @@ class HealthDetailViewController: UIViewController {
     }
     
     func configureChart() {
-        chartView.chartDescription?.enabled = false
         
+        chartView.chartDescription?.enabled = false
         chartView.dragEnabled = true
         chartView.setScaleEnabled(true)
-        chartView.pinchZoomEnabled = false
+        chartView.pinchZoomEnabled = true
         
         let xAxis = chartView.xAxis
         xAxis.labelPosition = .bottom
-        
-        chartView.rightAxis.enabled = false
-        
-        chartView.drawBarShadowEnabled = false
-        chartView.drawValueAboveBarEnabled = false
-        
-        chartView.maxVisibleCount = 60
-        
         xAxis.labelPosition = .bottom
         xAxis.labelFont = .systemFont(ofSize: 10)
         xAxis.granularity = 1
         xAxis.labelCount = 5
-        
         dayAxisValueFormatter = DayAxisValueFormatter(chart: chartView)
         dayAxisValueFormatter?.formatType = segmentedControl.selectedSegmentIndex
-        
         xAxis.valueFormatter = dayAxisValueFormatter
-        
-        let leftAxisFormatter = NumberFormatter()
-        leftAxisFormatter.minimumFractionDigits = 0
-        leftAxisFormatter.maximumFractionDigits = 1
-        leftAxisFormatter.negativeSuffix = ""
-        leftAxisFormatter.positiveSuffix = ""
-        
-        let leftAxis = chartView.leftAxis
-        leftAxis.labelFont = .systemFont(ofSize: 10)
-        leftAxis.labelCount = 8
-        leftAxis.valueFormatter = DefaultAxisValueFormatter(formatter: leftAxisFormatter)
-        leftAxis.labelPosition = .outsideChart
-        leftAxis.spaceTop = 0.15
-        leftAxis.axisMinimum = 0 // FIXME: HUH?? this replaces startAtZero = YES
-        
+
         let rightAxis = chartView.rightAxis
-        rightAxis.enabled = true
-        rightAxis.labelFont = .systemFont(ofSize: 10)
-        rightAxis.labelCount = 8
-        rightAxis.valueFormatter = leftAxis.valueFormatter
-        rightAxis.spaceTop = 0.15
+        rightAxis.removeAllLimitLines()
         rightAxis.axisMinimum = 0
+        rightAxis.drawLimitLinesBehindDataEnabled = true
         
-        let l = chartView.legend
-        l.horizontalAlignment = .left
-        l.verticalAlignment = .bottom
-        l.orientation = .horizontal
-        l.drawInside = false
-        l.form = .circle
-        l.formSize = 9
-        l.font = UIFont(name: "HelveticaNeue-Light", size: 11)!
-        l.xEntrySpace = 4
-        //        chartView.legend = l
-        
-        let marker = XYMarkerView(color: UIColor(white: 180/250, alpha: 1),
-                                  font: .systemFont(ofSize: 12),
-                                  textColor: .white,
-                                  insets: UIEdgeInsets(top: 8, left: 8, bottom: 20, right: 8),
-                                  xAxisValueFormatter: chartView.xAxis.valueFormatter!)
+        chartView.leftAxis.enabled = false
+
+        let marker = BalloonMarker(color: UIColor(white: 180/255, alpha: 1),
+                                   font: .systemFont(ofSize: 12),
+                                   textColor: .white,
+                                   insets: UIEdgeInsets(top: 8, left: 8, bottom: 20, right: 8))
         marker.chartView = chartView
         marker.minimumSize = CGSize(width: 80, height: 40)
         chartView.marker = marker
-    }
-    
-    func updateChartData() {
-        self.setDataCount(12, range: 50)
-    }
-    
-    func setDataCount(_ count: Int, range: UInt32) {
-        let start = 1
         
-        let yVals = (start..<start+count+1).map { (i) -> BarChartDataEntry in
-            let mult = range + 1
-            let val = Double(arc4random_uniform(mult))
-            if arc4random_uniform(100) < 25 {
-                return BarChartDataEntry(x: Double(i), y: val, icon: UIImage(named: "icon"))
-            } else {
-                return BarChartDataEntry(x: Double(i), y: val)
-            }
-        }
+        chartView.legend.form = .line
         
-        var set1: BarChartDataSet! = nil
-        if let set = chartView.data?.dataSets.first as? BarChartDataSet {
-            set1 = set
-            set1.replaceEntries(yVals)
-            chartView.data?.notifyDataChanged()
-            chartView.notifyDataSetChanged()
-        } else {
-            set1 = BarChartDataSet(entries: yVals, label: "Today")
-            set1.colors = ChartColorTemplates.material()
-            set1.drawValuesEnabled = false
-            
-            let data = BarChartData(dataSet: set1)
-            data.setValueFont(UIFont(name: "HelveticaNeue-Light", size: 10)!)
-            data.barWidth = 0.9
-            chartView.data = data
-        }
+        chartView.animate(xAxisDuration: 2.5)
     }
     
     @objc func changeSegment(_ segmentedControl: UISegmentedControl) {
@@ -202,7 +134,6 @@ class HealthDetailViewController: UIViewController {
             return
         }
         
-        var barWidth: Double = 0.85
         let endDate = Date()
         let calendar = Calendar.current
         var startDate = calendar.startOfDay(for: endDate)
@@ -224,35 +155,55 @@ class HealthDetailViewController: UIViewController {
                 return
             }
             
+//            let count = 45
+//            let range: UInt32 = 100
+//            let values = (0..<count).map { (i) -> ChartDataEntry in
+//                let val = Double(arc4random_uniform(range) + 3)
+//                return ChartDataEntry(x: Double(i), y: val)
+//            }
+            
             HealthKitService.getAllTheSamples(for: stepCountSampleType, startDate: startDate, endDate: endDate) { (samples, error) in
                 if let samples = samples {
                     var i = 0
-                    var entries: [BarChartDataEntry] = []
+                    var entries: [ChartDataEntry] = []
+                    var maxValue: Double = 0
                     for sample in samples {
                         if let quantitySample = sample as? HKQuantitySample {
                             let steps = quantitySample.quantity.doubleValue(for: .count())
-                            let entry = BarChartDataEntry(x: Double(i), y: steps, data: quantitySample.endDate)
+                            maxValue = max(maxValue, steps)
+                            let entry = ChartDataEntry(x: Double(i), y: steps, data: quantitySample.endDate)
                             entries.append(entry)
                             i += 1
                         }
                     }
                     
-                    if entries.count <= 2 {
-                        barWidth = 0.1
-                    }
-                    else if entries.count <= 10 {
-                        barWidth = 0.5
-                    }
-
-                    let dataSet = BarChartDataSet(entries: entries, label: "")
-                    dataSet.colors = ChartColorTemplates.material()
+                    let dataSet = LineChartDataSet(entries: entries, label: "")
+                    dataSet.drawIconsEnabled = false
+                    dataSet.mode = .cubicBezier
+                    dataSet.setColor(.black)
+                    dataSet.setCircleColor(.black)
+                    dataSet.drawCirclesEnabled = false
                     dataSet.drawValuesEnabled = false
+                    dataSet.circleRadius = 3
+                    dataSet.drawCircleHoleEnabled = false
+                    dataSet.valueFont = .systemFont(ofSize: 9)
+                    dataSet.formSize = 15
+                    dataSet.lineWidth = 0
                     
-                    let data = BarChartData(dataSet: dataSet)
+                    let gradientColors = [ChartColorTemplates.colorFromString("#00ff0000").cgColor,
+                                          ChartColorTemplates.colorFromString("#ffff0000").cgColor]
+                    let gradient = CGGradient(colorsSpace: nil, colors: gradientColors as CFArray, locations: nil)!
+                    
+                    dataSet.fillAlpha = 1
+                    dataSet.fill = Fill(linearGradient: gradient, angle: 90)
+                    dataSet.drawFilledEnabled = true
+                    
+                    let data = LineChartData(dataSet: dataSet)
                     data.setValueFont(UIFont(name: "HelveticaNeue-Light", size: 10)!)
-                    data.barWidth = barWidth
+                    maxValue *= 1.2
                     DispatchQueue.main.async {
                         self.dayAxisValueFormatter?.formatType = self.segmentedControl.selectedSegmentIndex
+                        self.chartView.rightAxis.axisMaximum = maxValue
                         self.chartView.data = data
                     }
                 }
