eafba8ca05271b913a1f87736681ee48f99b74a3
Hafiz Usama <hafizuamjad@gmail.com>
2020-12-06 22:52:26 -0800

    Auto select category type for activity sync from calendar

---
+++ Plot/Model/ActivityCategorySelector.swift
@@ -0,0 +1,34 @@
+//
+//  ActivityCategorySelector.swift
+//  Plot
+//
+//  Created by Hafiz Usama on 2020-12-06.
+//  Copyright © 2020 Immature Creations. All rights reserved.
+//
+
+import Foundation
+
+class ActivityCategorySelector {
+    static var keywordsMap = ["Exercise": ["fitness", "workout", "run", "training", "cycling", "hiit", "exercise"],
+                       "Work": ["meeting", "assignment", "project", "standup"],
+                       "Social": ["birthday", "party"],
+                       "Family": ["family"],
+                       "Personal": ["appointment", "consultation", "therapy"],
+                       "Meal": ["dinner", "lunch", "meal", "breakfast"]
+    ]
+    
+    class func selectCategory(for activity: Activity) -> String {
+        let text = "\(activity.name?.lowercased() ?? "") \(activity.notes?.lowercased() ?? "")"
+        let elements = text.split(separator: " ")
+        
+        for (category, keywords) in keywordsMap {
+            if elements.contains(where: { substring -> Bool in
+                return keywords.contains(String(substring))
+            }) {
+                return category
+            }
+        }
+        
+        return "Not Applicable"
+    }
+}
