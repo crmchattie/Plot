f2290155a932dcc6309676aa21f9b36de4fe8cb7
Cory McHattie <crmchattie@gmail.com>
2021-01-26 12:59:23 -0500

    Minor updates.

--- Plot/ActivityControllers/CreateNewActivityControllers/CreateActivityViewController.swift
+++ Plot/ActivityControllers/CreateNewActivityControllers/CreateActivityViewController.swift
@@ -216,7 +216,7 @@ class CreateActivityViewController: FormViewController {
                     cell.height = { return CGFloat(44) }
                     
                     row.title = "Cover Photo"
-                    cell.titleLeftMargin = 15.0
+                    row.cell.titleLabel?.textAlignment = .left
                     cell.titleLabel?.textColor = ThemeManager.currentTheme().generalSubtitleColor
                     
 //                    //  Construct the view for the cell
