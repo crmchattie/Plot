854b9961ad90968ff6ad74e55470ebea8e55ab32
Cory McHattie <crmchattie@gmail.com>
2022-08-25 16:00:14 -0400

    Commit file

---
+++ Plot/Settings/Menu/Time/Reminders/TaskAccountView.swift
@@ -0,0 +1,61 @@
+//
+//  CalendarAccountView.swift
+//  Plot
+//
+//  Created by Cory McHattie on 2/9/21.
+//  Copyright © 2021 Immature Creations. All rights reserved.
+//
+
+import UIKit
+
+class TaskAccountView: UIView {
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = ThemeManager.currentTheme().generalTitleColor
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.numberOfLines = 1
+        return label
+    }()
+    
+//    let infoLabel: UILabel = {
+//        let label = UILabel()
+//        label.textColor = ThemeManager.currentTheme().generalSubtitleColor
+//        label.font = UIFont.preferredFont(forTextStyle: .callout)
+//        label.numberOfLines = 1
+//        return label
+//    }()
+    
+    let accountImageView = UIImageView(cornerRadius: 8)
+    let statusImageView = UIImageView(cornerRadius: 8)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clear
+//        let backgroundView = UIView()
+//        addSubview(backgroundView)
+//        backgroundView.fillSuperview()
+//        backgroundView.backgroundColor = ThemeManager.currentTheme().cellBackgroundColor
+//
+//        backgroundView.roundCorners(corners: [.topLeft, .topRight], radius: 10)
+        
+        accountImageView.constrainWidth(40)
+        accountImageView.constrainHeight(40)
+        
+        statusImageView.constrainWidth(25)
+        statusImageView.constrainHeight(25)
+        
+//        let labelStack = VerticalStackView(arrangedSubviews: [nameLabel], spacing: 0)
+        
+        let stackView = UIStackView(arrangedSubviews: [accountImageView, nameLabel, UIView(), statusImageView])
+        stackView.spacing = 10
+        stackView.alignment = .center
+        addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 5, left: 10, bottom: 5, right: 10))
+
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+ 
+}
